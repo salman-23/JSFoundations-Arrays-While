2aa6b7f18ea094c61f4e9dd25d9570f0b1aa7dbf
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-01-11 15:20:28 +0300

    solved the areey functions array

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -10,6 +10,11 @@
  */
 function isArrayLengthOdd(numbers) {
   // Your code here
+  if (numbers.length % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -24,6 +29,11 @@ function isArrayLengthOdd(numbers) {
  */
 function isArrayLengthEven(numbers) {
   // Your code here
+  if (numbers.length % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -36,6 +46,8 @@ function isArrayLengthEven(numbers) {
  */
 function addLailaToArray(instructors) {
   // Your code here
+  instructors.push("Laila");
+  return instructors;
 }
 
 /**
@@ -48,6 +60,7 @@ function addLailaToArray(instructors) {
  */
 function eliminateTeam(teams) {
   // Your code here
+  return teams.pop();
 }
 
 /**
@@ -62,6 +75,14 @@ function eliminateTeam(teams) {
  */
 function secondHalfOfArrayIfItIsEven(fruits) {
   // Your code here
+  if (fruits.length % 2 === 0) {
+    let fruitsHalf = Math.ceil(fruits.length / 2);
+    let newFruits = fruits.slice(fruitsHalf, fruits.length);
+    return newFruits;
+  } else {
+    fruits = [];
+    return fruits;
+  }
 }
 
 /**
@@ -80,6 +101,10 @@ function secondHalfOfArrayIfItIsEven(fruits) {
  */
 function youGottaCalmDown(shout) {
   // Your code here
+  while (shout.endsWith("!!")) {
+    shout = shout.slice(0, -1);
+  }
+  return shout;
 }
 
 module.exports = {
